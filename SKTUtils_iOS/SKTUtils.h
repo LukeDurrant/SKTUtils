21e4c30f2317e5af1e3ff79304044b21e50bf3d0
lukedurrant <contact@lukedurrant.com>
2019-06-05 22:40:33 +1000

    Add shared scheme for carthage

---
+++ SKTUtils_iOS/SKTUtils.h
@@ -0,0 +1,19 @@
+//
+//  SKTUtils.h
+//  SKTUtils
+//
+//  Created by Luke Durrant on 5/6/19.
+//  Copyright © 2019 Razeware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SKTUtils.
+FOUNDATION_EXPORT double SKTUtilsVersionNumber;
+
+//! Project version string for SKTUtils.
+FOUNDATION_EXPORT const unsigned char SKTUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SKTUtils/PublicHeader.h>
+
+
---
+++ SKTUtils_tvOS/SKTUtils_tvOS.h
@@ -0,0 +1,19 @@
+//
+//  SKTUtils_tvOS.h
+//  SKTUtils_tvOS
+//
+//  Created by Luke Durrant on 5/6/19.
+//  Copyright © 2019 Razeware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SKTUtils_tvOS.
+FOUNDATION_EXPORT double SKTUtils_tvOSVersionNumber;
+
+//! Project version string for SKTUtils_tvOS.
+FOUNDATION_EXPORT const unsigned char SKTUtils_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SKTUtils_tvOS/PublicHeader.h>
+
+
